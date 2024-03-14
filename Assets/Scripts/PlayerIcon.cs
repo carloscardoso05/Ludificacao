97465103e9b27707439526980658736dad17b072
Carlos Cardoso <cvcardos5@gmail.com>
2024-03-13 21:03:14 -0300

    animação do jogador atual

--- Assets/Scripts/PlayerIcon.cs
+++ Assets/Scripts/PlayerIcon.cs
@@ -1,17 +1,51 @@
+using System;
+using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.U2D.Animation;
 
-[RequireComponent(typeof(SpriteRenderer))]
-[RequireComponent(typeof(SpriteResolver))]
-[RequireComponent(typeof(SpriteLibrary))]
-public class PlayerIcon : MonoBehaviour {
-    [SerializeField] private SpriteResolver resolver;
-    
-    private void Start() {
+public class PlayerIcon : MonoBehaviour
+{
+    [SerializeField] private GameObject BlueIcon;
+    [SerializeField] private GameObject RedIcon;
+    [SerializeField] private GameObject GreenIcon;
+    [SerializeField] private GameObject YellowIcon;
+    private GameObject currentIcon;
+
+    private void Start()
+    {
         GameManager.I.OnTurnChanged += ChangeIconColor;
+        currentIcon = GetIcon(GameManager.I.colorsManager.currentColor);
     }
 
-    private void ChangeIconColor(object sender, GameColor newColor) {
-        resolver.SetCategoryAndLabel("player", newColor.ToString());
+    private void ChangeIconColor(object sender, GameColor newColor)
+    {
+        currentIcon = GetIcon(GameManager.I.colorsManager.currentColor);
+        foreach (Transform child in transform)
+        {
+            child.localScale = Vector3.one;
+        }
+    }
+    private void Update()
+    {
+        if (currentIcon != null)
+        {
+            var scale = currentIcon.transform.localScale;
+            print(scale);
+            var x = (float)(math.sin(Time.time) + 2) / 3;
+            var y = (float)(math.sin(Time.time) + 2) / 3;
+            var newScale = new Vector3(x, y, 1);
+            currentIcon.transform.localScale = newScale;
+        }
+    }
+
+    private GameObject GetIcon(GameColor color)
+    {
+        return color switch
+        {
+            GameColor.Blue => BlueIcon,
+            GameColor.Red => RedIcon,
+            GameColor.Green => GreenIcon,
+            GameColor.Yellow => YellowIcon,
+            _ => throw new ArgumentException("Branco não é válido")
+        };
     }
 }
